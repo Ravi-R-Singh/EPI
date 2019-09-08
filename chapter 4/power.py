2ec540e0796999ab5987cc82faee97534b5cccdc
Ravi <r.singh98@hotmail.com>
2019-09-07 20:06:45 -0400

    brute force of 4.7 complete

--- chapter 4/power.py
+++ chapter 4/power.py
@@ -1,3 +1,8 @@
 import sys
-def power(float: x) -> float:
-    
+def power(x: float, y: int) -> float:
+    temp = x
+    for i in range(y-1):
+        x *= temp
+    return x
+
+print(power(float(sys.argv[1]),int(sys.argv[2])))
