62948f668f72e229c8416f128f8ae70c92a425bb
Ravi <r.singh98@hotmail.com>
2019-09-07 21:57:12 -0400

    optimized 4.7 complete

--- chapter 4/power.py
+++ chapter 4/power.py
@@ -1,8 +1,14 @@
 import sys
-def power(x: float, y: int) -> float:
-    temp = x
-    for i in range(y-1):
-        x *= temp
-    return x
 
-print(power(float(sys.argv[1]),int(sys.argv[2])))
+def power(x: float, y: int) -> float:
+    result, power = 1.0, y
+    if y < 0:
+        power,x = -power, 1.0/x
+    while power:
+        if power & 1:
+            result *= x
+        x, power = x * x, power >> 1
+    return result
+
+
+print(power2(float(sys.argv[1]),int(sys.argv[2])))
